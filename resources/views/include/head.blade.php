565778b912aaaf0bcb2fee3ae3db61b5ac22147b
Engbakri <bakricfcss@gmail.com>
2022-06-04 13:34:41 +0300

    ecommrce theme

---
+++ resources/views/include/head.blade.php
@@ -0,0 +1,23 @@
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Ezone - eCommerce HTML5 Template</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('assets/img/favicon.png')}}">
+    
+    <!-- all css here -->
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/magnific-popup.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/animate.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/owl.carousel.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/themify-icons.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/pe-icon-7-stroke.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/icofont.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/meanmenu.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/bundle.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/responsive.css') }}">
+    <script src="{{ asset('assets/js/vendor/modernizr-3.11.7.min.js')}}"></script>
+</head>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+@include('include.head')
+
+<body>
+
+    @include('include.header2')
+         
+       @yield('content')
+
+    @include('include.footer')
+    
+</body>
+</html>
