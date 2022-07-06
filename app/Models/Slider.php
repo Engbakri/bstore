f96b150f47b6bb018a5b456a09875fda32dffb9a
Engbakri <bakricfcss@gmail.com>
2022-07-06 10:35:21 +0400

    checkout

--- app/Models/Slider.php
+++ app/Models/Slider.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Slider extends Model
 {
     use HasFactory;
+    protected $table = 'sliders';
 
     protected $fillable = ['slider_image','slider_title','slider_description'];
 }
